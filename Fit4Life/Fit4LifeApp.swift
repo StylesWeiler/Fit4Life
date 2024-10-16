fbdc895bd3b430a9e766104c8488eaf83cc8f51d
StylesWeiler <95368364+StylesWeiler@users.noreply.github.com>
2024-10-15 22:05:26 -0600

    Initial Commit

---
+++ Fit4Life/Fit4LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit4LifeApp.swift
+//  Fit4Life
+//
+//  Created by Todd Weiler on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Fit4LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
